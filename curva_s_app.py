5df78d3445c0796475f014a4878e5bbc8e672aba
gustavoowcampos123 <gugaguga.henrique816@gmail.com>
2024-09-28 22:08:48 -0300

    Update curva_s_app.py

--- curva_s_app.py
+++ curva_s_app.py
@@ -196,8 +196,8 @@ if uploaded_file is not None and start_date and end_date:
         else:
                        timeline, curva_s, delta = generate_s_curve(df, start_date, end_date)
             
-            st.write("Curva S:")
-            plot_s_curve(timeline, curva_s)
+st.write("Curva S:")
+plot_s_curve(timeline, curva_s)
             
             # Exportar o Excel e fornecer o download
             excel_data = export_to_excel(df, caminho_critico, curva_s, delta, timeline)
