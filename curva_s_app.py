1817e68027ac98bcbd7490497a1e9b10e7863ba1
gustavoowcampos123 <gugaguga.henrique816@gmail.com>
2024-09-29 21:25:54 -0300

    Update curva_s_app.py

--- curva_s_app.py
+++ curva_s_app.py
@@ -239,7 +239,7 @@ def calculate_critical_path(df):
         return [], atividades_sem_predecessora
 
 # Interface Streamlit
-st.title('Gerador de Curva S e Caminho Crítico com Alerta de Atraso e Relatório PDF')
+st.title('AWPlan - A ferramenta de Controle de Cronograma')
 
 uploaded_file = st.file_uploader("Escolha o arquivo Excel do cronograma", type="xlsx")
 
